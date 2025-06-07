ed3979068f345b2184014f466fdb9b3bd90b0dcf
sasank pankam <sasaankme@gmail.com>
2025-06-07 12:08:31 +0530

    Created basic structure of project.

---
+++ loan/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from ale_assignment.loan.views import ProtectedView
+
+
+# change.
+urlpatterns = [
+    path("register", ProtectedView.as_view(), name="token_obtain_pair"),
+    path("check-eligibility", ProtectedView.as_view(), name="token_obtain_pair"),
+    path("create-loan", ProtectedView.as_view(), name="token_obtain_pair"),
+    path("view-loan/<int:loan_id>", ProtectedView.as_view(), name="token_obtain_pair"),
+    path(
+        "view-loans/<int:customer_id>",
+        ProtectedView.as_view(),
+        name="token_obtain_pair",
+    ),
+]
