f97485db6af7e5c2ce4a95c34ee7553f52441c63
sasank pankam <sasaankme@gmail.com>
2025-06-08 14:54:49 +0530

    Removed prints for clean ness

--- upload.py
+++ upload.py
@@ -39,7 +39,7 @@ def import_data(csv_path, headers, serializer):
 
         for row in reader:
             if len(row) != len(headers):
-                print(f"Skipping invalid row (wrong length): {row}")
+                # print(f"Skipping invalid row (wrong length): {row}")
                 failed += 1
                 continue
             data = {}
@@ -51,15 +51,16 @@ def import_data(csv_path, headers, serializer):
                 if obj.is_valid():
                     obj.save()
                 success += 1
-            except serializers.ValidationError as ve:
-                print(f"Validation failed for row {row}: {ve}")
+            except serializers.ValidationError:
+                # print(f"Validation failed for row {row}: {ve}")
                 failed += 1
-            except IntegrityError as ve:
-                print(f"Id already exists for row {row}: {ve}")
+            except IntegrityError:
+                # print(f"Id already exists for row {row}: {ve}")
                 failed += 1
-            except Exception as e:
-                print("Cannot upload user, for reason:", e)
-                print(data)
+            except Exception:
+                # print("Cannot upload user, for reason:", e)
+                # print(data)
+                pass
 
 
 import_data("./customer_data.csv", CUSTOMER_HEADERS, CustomerSerializer)
